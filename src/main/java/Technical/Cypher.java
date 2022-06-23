5d354536ab54a7da95c3b0e0a74ab8ef79282f5c
Daniel <stickbugdanny@gmail.com>
2022-06-23 15:58:16 +0300

    cypher done

--- src/main/java/Technical/Cypher.java
+++ src/main/java/Technical/Cypher.java
@@ -1,4 +1,39 @@
 package Technical;
 
-public class Cypher {
-}
+import lombok.Builder;
+import lombok.Data;
+
+
+
+    @Data
+    @Builder(toBuilder = true)
+
+
+    public class Cypher {
+
+        /**de encoding method*/
+        private static String applyCypherKey(String string) {
+            char p = 'P';
+
+            String outputString = "";
+            int l = string.length();
+
+            //apply key
+            for (int i = 0; i < l; i++) {
+                outputString = outputString + (char)(string.charAt(i)^p);
+            }
+
+            return outputString;
+        }
+
+        public static String encode(String string) {
+            return applyCypherKey(string);
+        }
+
+        public static String decode(String string) {
+            return applyCypherKey(string);
+        }
+    }
+
+
+
