9088b15d098ee83c9eef9108b77ce8e75a3685b6
Daniel <stickbugdanny@gmail.com>
2022-06-23 16:30:30 +0300

    sender & receiver complete

--- src/main/java/Processing/Receiver.java
+++ src/main/java/Processing/Receiver.java
@@ -1,4 +1,37 @@
 package Processing;
 
+import Technical.Message;
+import Technical.Packet;
+import java.util.Random;
+
+enum cTypes {
+    GET_PRODUCT_AMOUNT,
+    SET_PRODUCT_AMOUNT,
+    SET_PRODUCT_DESCRIPTION,
+    GET_PRODUCT_DESCRIPTION,
+    SET_PRODUCT_PRICE,
+    GET_PRODUCT_PRICE,
+    GET_PRODUCT,
+    ADD_PRODUCT,
+    ADD_PRODUCT_NAME,
+    ADD_PRODUCT_TO_GROUP
+}
+
+/**the actual receiver*/
 public class Receiver {
+    public static byte[] receive() {
+        Random random = new Random();
+
+        int command = random.nextInt(cTypes.values().length);//random command
+
+        String commandMessage = (cTypes.values()[command]).toString();
+        Message testMessage = new Message(command,1, commandMessage);
+
+        long bPaketID = random.nextLong();//generate a random Long
+
+        Packet packet = new Packet((byte)1, bPaketID, testMessage);
+
+        byte[] packetToBytes = packet.toPacket();
+        return packetToBytes;
+    }
 }
--- src/main/java/Processing/Sender.java
+++ src/main/java/Processing/Sender.java
@@ -1,4 +1,18 @@
 package Processing;
 
+
+import Technical.Message;
+import Technical.Packet;
+
+/** packs the already encoded answer to the client*/
 public class Sender {
+    public static byte[] packResponse(Packet packet) {
+        String response = "OK";
+
+        Message answer = new Message(packet.getBMsq().getCType(), packet.getBMsq().getbUserId(), response);
+        Packet respPacket = new Packet((byte) 1, packet.getbPaketID(), answer);
+
+        byte[] encodedPacket = respPacket.toPacket();
+        return encodedPacket;
+    }
 }
