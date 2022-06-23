4bcbdc653c1c3a108526871a7e685b2c3406df33
Daniel <stickbugdanny@gmail.com>
2022-06-23 17:10:14 +0300

    main works

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import Processing.Processor;
+import Processing.usableNetwork;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
-
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        for(int i = 0; i < 20; i++)
+            executorService.submit(()->{
+                usableNetwork network = new usableNetwork();
+                network.receiveMessage();
+            });
+        try{
+            executorService.shutdown();
+            while(!executorService.awaitTermination(24L, TimeUnit.HOURS)){
+                System.out.println("Please wait");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Processor.shutdown();
+        System.out.println("End");
     }
 }
